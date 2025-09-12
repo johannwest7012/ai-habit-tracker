f7019f821d90ee30b9ce5ad92e912adfe5366b59
Johann West <johannwest@gmail.com>
2025-09-12 14:21:51 -0700

    1.11 dev complete, still need to qa, fixed an AuthContext error

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
+import { AuthProvider } from './src/contexts/AuthContext';
 import { createQueryClient, setupQueryPersistence } from './src/services/api/queryClient';
 
 // Initialize query client
@@ -17,8 +18,10 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RootNavigator />
-      <StatusBar style="auto" />
+      <AuthProvider>
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
